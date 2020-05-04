c85b2f9df4d44226442590ef49866ddb41929fdd
manuelosorio <manuelosorio.mkdesignz@gmail.com>
2020-05-03 21:02:53 -0400

    Setup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UserComponent } from './user/user.component';
+import {HomeComponent} from './home/home.component';
+import {ListComponent} from './list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'lists', component: ListComponent},
+  {path: 'users', component: UserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
