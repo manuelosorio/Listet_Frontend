bf39b72a0602d70e6dd4661ae30ff181db494c9e
manuelosorio <manuelosorio.mkdesignz@gmail.com>
2020-05-03 21:05:50 -0400

    Get user info from backend server

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UsersService) {}
+  users: object;
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
+      console.log(this.users);
+    });
   }
 
 }
