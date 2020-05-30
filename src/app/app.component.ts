d278d8a7634e1977dc4af171d32276d920407638
manuelosorio <manuelosorio.mkdesignz@gmail.com>
2020-05-30 18:28:41 -0400

    Fix auth check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {UsersService} from './users.service';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +7,4 @@ import {UsersService} from './users.service';
 })
 export class AppComponent {
   title = 'todo-list';
-  authenticated;
-
-  constructor(
-    private userService: UsersService,
-  ) {
-    this.userService.authenticated.subscribe(authenticated => {
-      this.authenticated = authenticated;
-    });
-  }
 }
