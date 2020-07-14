9514697246fe3a40fb7f5e1c3a989a9c7657012d
ManuelOsorio <manuelosorio.mkdesignz@gmail.com>
2020-07-14 05:03:46 -0400

    Separate login form from navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { UserComponent } from './user/user.component';
 import {HomeComponent} from './home/home.component';
 import {ListComponent} from './list/list.component';
 import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'lists', component: ListComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'users', component: UserComponent },
-  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
