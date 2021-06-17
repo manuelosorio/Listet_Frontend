47db21169dbbc941cc93199f8a2c6c5ec0ab82f3
ManuelOsorio <manuelosorio.mkdesignz@gmail.com>
2021-06-16 23:21:42 -0400

    Remove test.css
    I was just trying something  I saw in someone else project and forgot to revert.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CanonicalService } from './shared/canonical.service';
-import './test.css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
-  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit{
   title = 'Listet';
