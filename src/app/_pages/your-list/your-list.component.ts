eccbb88090165a85fcb7c4f9ff615a5aff5e14ef
ManuelOsorio <manuelosorio.mkdesignz@gmail.com>
2021-06-19 08:59:35 -0400

    Add 'your list' component and route
    Also added 'your list' to navigation as an authed route and removed 'users' route

---
+++ src/app/_pages/your-list/your-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-your-list',
+  templateUrl: './your-list.component.html',
+  styleUrls: ['./your-list.component.sass']
+})
+export class YourListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
