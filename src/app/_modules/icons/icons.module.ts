d4f5224437dfefe1e74302f5874fdd561ec0433a
ManuelOsorio <manuelosorio.mkdesignz@gmail.com>
2021-07-12 01:52:35 -0400

    Add circle user icon
    Uses icon from feather icons pull request: [#692](https://github.com/feathericons/feather/pull/692)

--- src/app/_modules/icons/icons.module.ts
+++ src/app/_modules/icons/icons.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertCircle, ArrowLeft, Check, CheckSquare, ChevronLeft, Clock, Edit, Frown,
   Github, Heart, Linkedin, Menu, MoreVertical, Search, Square, Trash, Twitter, User, X, XCircle } from 'angular-feather/icons';
-import {FeatherModule} from 'angular-feather';
-
+import { UserCircle } from '../../shared/other-icons';
+import { FeatherModule } from 'angular-feather';
 
 const icons: any = {
   AlertCircle,
@@ -24,6 +24,7 @@ const icons: any = {
   Trash,
   Twitter,
   User,
+  UserCircle,
   X,
   XCircle
 };
