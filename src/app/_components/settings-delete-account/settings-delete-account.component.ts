fcfe35c505b9923961ab157de3c45863e6830d95
ManuelOsorio <manuelosorio.mkdesignz@gmail.com>
2021-07-18 02:22:00 -0400

    Move SettingsComponent from AppModule to SettingsModule

---
+++ src/app/_components/settings-delete-account/settings-delete-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-delete-account',
+  templateUrl: './settings-delete-account.component.html',
+  styleUrls: ['./settings-delete-account.component.sass']
+})
+export class SettingsDeleteAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
