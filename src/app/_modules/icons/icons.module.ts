4b1c6e7140013e5b05215c8ea6426cc114f48e9b
ManuelOsorio <manuelosorio.mkdesignz@gmail.com>
2021-08-21 03:18:39 -0400

    Slight changes to _form.sass, settings.component.sass and _buttons.sass, add 'save' feather icon

--- src/app/_modules/icons/icons.module.ts
+++ src/app/_modules/icons/icons.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertCircle, ArrowLeft, Check, CheckSquare, ChevronLeft, Clock, Edit, Frown,
-  Github, Heart, Info, Linkedin, Menu, MoreVertical, Search, Square, Trash, Twitter, User, X, XCircle } from 'angular-feather/icons';
+  Github, Heart, Info, Linkedin, Menu, MoreVertical, Save, Search, Square, Trash, Twitter, User, X, XCircle } from 'angular-feather/icons';
 import { UserCircle } from '../../shared/other-icons';
 import { FeatherModule } from 'angular-feather';
 
-const icons: any = {
+const icons: { [key: string]: string } = {
   AlertCircle,
   ArrowLeft,
   Check,
@@ -20,6 +20,7 @@ const icons: any = {
   Linkedin,
   Menu,
   MoreVertical,
+  Save,
   Search,
   Square,
   Trash,
