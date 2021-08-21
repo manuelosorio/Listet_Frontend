2ecb99c3b6c75d4617368660812a5a160b94e3a8
ManuelOsorio <manuelosorio.mkdesignz@gmail.com>
2021-08-21 03:32:53 -0400

    Add user profile information form

--- src/app/_components/settings-profile/settings-profile.component.ts
+++ src/app/_components/settings-profile/settings-profile.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UsersService } from '../../_services/users.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-settings-profile',
   templateUrl: './settings-profile.component.html',
   styleUrls: ['./settings-profile.component.sass']
 })
-export class SettingsProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SettingsProfileComponent implements OnInit, OnDestroy {
+  public profileForm: FormGroup
+  private updateAccountInfo$: Subscription;
+  constructor(private userService: UsersService,
+              private formBuilder: FormBuilder) {
+    this.profileForm = this.formBuilder.group({
+      email: [],
+      firstName: [],
+      lastName: [],
+    });
   }
 
+  ngOnInit(): void {
+    this.userService.userInfo$.subscribe(res => {
+      console.log(res);
+      this.profileForm.setValue({
+        email: res.email,
+        firstName: res.firstName,
+        lastName: res.lastName
+      });
+    });
+  }
+  submit(data) {
+    this.updateAccountInfo$ = this.userService.updateAccountInfo(data);
+  }
+  ngOnDestroy(): void {
+  }
+  get firstName() {
+    return this.profileForm.get('firstName');
+  }
+  get lastName() {
+    return this.profileForm.get('lastName');
+  }
+  get email() {
+    return this.profileForm.get('email');
+  }
 }
