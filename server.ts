0a6025eb9c602362bde9f31e56b1c12b9f826cbc
Manuel Osorio <manuelosorio@users.noreply.github.com>
2021-10-04 12:10:39 -0400

    Remove serverless so SSR works in Phusion Passenger.

--- server.ts
+++ server.ts
@@ -39,7 +39,7 @@ global['window'] = win;
 global['document'] = win.document;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = environment.production ? '1' : '0';
 // The Express app is exported so that it can be used by serverless Functions.
-export function app() {
+
   const server = express();
   const distFolder = join(process.cwd(), 'dist/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
@@ -64,27 +64,9 @@ export function app() {
     res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
   });
 
-  return server;
-}
-
-function run() {
-  const port = process.env.PORT || 4000;
-
-  // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+  server.set('port', process.env.PORT || 4000);
+  server.listen(server.get('port'), () => {
+    console.log(`Node Express server listening on port ${server.get('port')}`);
   });
-}
-
-// Webpack will replace 'require' with '__webpack_require__'
-// '__non_webpack_require__' is a proxy to Node 'require'
-// The below code is to ensure that the server is run only when not requiring the bundle.
-declare const __non_webpack_require__: NodeRequire;
-const mainModule = __non_webpack_require__.main;
-const moduleFilename = mainModule && mainModule.filename || '';
-if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
-  run();
-}
 
 export * from './src/main.server';
