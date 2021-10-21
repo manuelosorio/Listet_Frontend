0388a7dbaa6de3b8479bc2737d872da24ef9c30e
Manuel Osorio <manuelosorio@users.noreply.github.com>
2021-10-21 03:40:03 -0400

    Hide searchbar in certain routes
    Also make changes to code style and remove redundant code in nav.component.ts

--- src/app/_components/search/search.component.ts
+++ src/app/_components/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from "@angular/router";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { filter } from "rxjs/operators";
 
 @Component({
   selector: 'app-search',
@@ -10,6 +12,17 @@ export class SearchComponent implements OnInit {
   public searchForm: FormGroup;
   public windowWidth: number
 
+  private hidePaths = [
+    '/create-list',
+    '/login',
+    '/register',
+    '/forgot-password',
+    '/reset-password',
+    '/reset-password/**',
+    '/settings',
+    '/settings/**'
+  ];
+  public hideSearch: boolean;
   constructor(public router: Router,
               private formBuilder: FormBuilder) {
     this.searchForm = this.formBuilder.group({
@@ -17,6 +30,16 @@ export class SearchComponent implements OnInit {
         Validators.minLength(1)
       ]]
     })
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+      for (const path of this.hidePaths) {
+        if (event.urlAfterRedirects.indexOf(path) === 0) {
+          this.hideSearch = true;
+          break;
+        }
+        this.hideSearch = false;
+      }
+    })
+    this.windowWidth = window.innerWidth;
   }
 
   ngOnInit(): void {
