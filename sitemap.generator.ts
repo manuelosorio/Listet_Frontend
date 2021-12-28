d49ed937f4b35b63c6f01d425cd1db8fe6e816df
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2021-12-27 22:04:01 -0500

    add sitemap generator

---
+++ sitemap.generator.ts
@@ -0,0 +1,17 @@
+const SitemapGenerator = require('sitemap-generator');
+// create generator
+const generator = SitemapGenerator(process.env.URL, {
+  stripQuerystring: true,
+  lastMod: true,
+  filepath: './dist/browser/assets/sitemap.xml'
+});
+
+// register event listeners
+generator.on('done', () => {
+  // sitemaps created
+});
+generator.on('error', (err) => {
+  console.error(err);
+});
+// start the crawler
+generator.start();
