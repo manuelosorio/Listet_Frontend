8be1e3a4b55ccaafe0f063e4d70866078481879d
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2023-06-28 14:47:24 -0400

    Update models
    - List
    - List Items
    - User

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,7 +1,14 @@
+import { EndpointResponse } from './response/endpoint.response';
+
 export interface UserModel {
-  firstName: string,
-  lastName: string,
-  email: string,
-  username: string,
-  avatar ?: string
+  firstName: string;
+  lastName: string;
+  email?: string;
+  username: string;
+  avatar?: string;
+}
+
+export interface UserResponse extends UserModel, EndpointResponse {
+  authenticated: boolean;
+  verified?: boolean;
 }
