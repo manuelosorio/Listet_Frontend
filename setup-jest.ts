dbeef1ecd7bc78636303ee3d30f9ced2aee77072
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2026-03-08 19:15:35 -0400

    Update test setup

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,9 +1,10 @@
-import 'jest-preset-angular/setup-jest';
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { IconsModule } from '@modules/icons/icons.module';
 
 // Configure global testing setup
+setupZoneTestEnv();
 beforeAll(() => {
   TestBed.configureTestingModule({
     imports: [IconsModule],
